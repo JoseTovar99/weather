ef58cbd46a90b0bd03d44acb98940b879b08864d
JoseTovar510 <tovarjose1013@gmail.com>
2021-10-14 23:24:16 -0700

    Added Enter keyup function

--- main.js
+++ main.js
@@ -21,13 +21,13 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed); //It should show the Place, sunny or cloudy, temperature, and wind speed
-        document.querySelector(".city").innerText = "Weather in " + name; //Show the place in the website
+        document.querySelector(".city").innerText = name; //Show the place in the website
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/"+ icon + ".png"
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "mph";
+        //document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')"  **If I want to change background image on every new search**
     },
     search: function () {
         this.fetchWeather(document.getElementById("textEntry").value)
@@ -36,4 +36,10 @@ let weather = {
 
 document.querySelector(".inputText button").addEventListener("click", function () {
     weather.search();
+})
+
+document.getElementById("textEntry").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
 })
